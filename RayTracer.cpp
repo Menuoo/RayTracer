9ad33c8d511d29f1552fcb7d8a71b39e48b84b8a
Menuoo <lietuvis7080@gmail.com>
2025-10-18 23:12:19 +0300

    NEXT WEEK started!
    -- done up to 3.5

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -6,6 +6,7 @@
 #include "material.h"
 #include "sphere.h"
 
+#include "aabb.h"
 
 int main()
 {
@@ -27,7 +28,8 @@ int main()
                     // diffuse
                     auto albedo = color::random() * color::random();
                     sphere_material = make_shared<lambertian>(albedo);
-                    world.add(make_shared<sphere>(center, 0.2, sphere_material));
+                    auto center2 = center + vec3(0, random_double(0, .5), 0);
+                    world.add(make_shared<sphere>(center, center2, 0.2, sphere_material));
                 }
                 else if (choose_mat < 0.95) {
                     // metal
@@ -61,7 +63,7 @@ int main()
 
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
-    cam.samples_per_pixel = 40;
+    cam.samples_per_pixel = 10;
     cam.max_depth = 10;
 
     cam.vfov = 20;
