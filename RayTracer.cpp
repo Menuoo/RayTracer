2cffaaafc35c42eed84ccee545f0f59413b5a921
Menuoo <lietuvis7080@gmail.com>
2025-10-19 17:47:30 +0300

    lost in the sauce
    -- BVH issues

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -1,12 +1,13 @@
 #include "rtweekend.h"
 
+#include "bvh.h"
 #include "camera.h"
 #include "hittable.h"
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
 
-#include "aabb.h"
+//#include "aabb.h"
 
 int main()
 {
@@ -56,6 +57,9 @@ int main()
     auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
     world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
+
+
+    world = hittable_list(make_shared<bvh_node>(world));
 
 
     // Camera setup
