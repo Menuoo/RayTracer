1e36c961029f34fc31feab32c4ecbf4a1a8950c3
Menuoo <lietuvis7080@gmail.com>
2025-10-21 12:21:56 +0300

    up to 4.4 (theNextWeek)

--- RayTracer.cpp
+++ RayTracer.cpp
@@ -6,16 +6,16 @@
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
+#include "texture.h"
 
-//#include "aabb.h"
 
-int main()
+void bouncing_spheres()
 {
     // World
     hittable_list world;
 
-    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
+    world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
 
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
@@ -78,4 +78,43 @@ int main()
     cam.focus_dist = 10.0;
 
     cam.render(world);
+}
+
+void checkered_spheres() {
+
+    hittable_list world;
+
+    auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
+
+    world.add(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
+    world.add(make_shared<sphere>(point3(0,  10, 0), 10, make_shared<lambertian>(checker)));
+
+
+    // Camera setup
+    camera cam;
+
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
+    cam.samples_per_pixel = 10;
+    cam.max_depth = 10;
+
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
+
+    cam.defocus_angle = 0;
+    // cam.focus_dist = 10.0; this is the implicit value though so idk why we doing allat
+
+    cam.render(world);
+}
+
+
+
+
+int main() {
+    switch (2) {
+        case 1: bouncing_spheres();  break;
+        case 2: checkered_spheres(); break;
+    }
 }
